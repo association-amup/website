f842ad8cc8445c1037943fce391a4c3ab4f65292
Hamza Haddani <hhamza@student.1337.ma>
2024-09-18 17:09:15 +0100

    chore: Vite boilerplate cleanup
    - Cleanup vite boilerplate
    - Update .gitignore
    - Install Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<App />} />
+				<Route path="/" element={<Home />} />
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>,
