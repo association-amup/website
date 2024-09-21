258f5dc3d87ef22ff6a821106c078db7108892d0
Hamza Haddani <hhamza@student.1337.ma>
2024-09-21 16:31:04 +0100

    wip: Update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
 	return (
-		<div>Acceuil</div>
+		<div className="mt-10">
+			<h1 className="text-center text-2xl font-bold">Association Marocaine des Usagers de la Psychiatrie</h1>
+		</div>
 	);
 }
